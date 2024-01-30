f0bdbf8017a810ccc808291a1b2255ad57d723eb
Patrick Dubroy <pdubroy@gmail.com>
2024-01-30 08:12:19 +0100

    Clean up demo, add serve script

--- scripts/serve.ts
+++ scripts/serve.ts
@@ -1,9 +1,24 @@
 const port = process.env.PORT || 3000;
 
+const transpiler = new Bun.Transpiler();
+
+async function serveTS(filename: string): Promise<Response> {
+  const source = await Bun.file(filename).text();
+  return new Response(await transpiler.transform(source));
+}
+
 try {
   Bun.serve({
-    fetch(_req: Request): Response | Promise<Response> {
-      return new Response(Bun.file("index.html"));
+    async fetch(req: Request): Promise<Response> {
+      const url = new URL(req.url);
+      switch (url.pathname) {
+        case "/":
+          return new Response(Bun.file("demo/index.html"));
+        case "/app.ts":
+          return serveTS("demo/app.ts");
+        default:
+          return new Response("404!", { status: 404 });
+      }
     },
     port,
   });
