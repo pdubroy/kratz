14319c85999f861877aad618fb25fa6b1169e45f
Patrick Dubroy <pdubroy@gmail.com>
2024-02-02 13:33:03 +0100

    Proper translation to/from zero-center coordinates

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-const WIDTH = 480 * 2;
-const HEIGHT = 360 * 2;
+const WIDTH = 480;
+const HEIGHT = 360;
 
 function checkNotNull<T>(x: T): NonNullable<T> {
   if (x == null) {
@@ -35,13 +35,13 @@ export class Stage {
 
   constructor(canvas?: HTMLCanvasElement) {
     this.view = canvas || document.createElement("canvas");
-    this.view.width = WIDTH / 2;
-    this.view.height = HEIGHT / 2;
+    this.view.width = WIDTH;
+    this.view.height = HEIGHT;
     this.view.style.border = "1px solid #aaa";
   }
 
   async addSprite(url: string, name?: string): Promise<Sprite> {
-    const sprite = new Sprite();
+    const sprite = new Sprite(this);
     const k = name || `sprite${this.nextSpriteId++}`;
     this.sprites.set(k, sprite);
     await sprite.addCostume(url);
@@ -54,14 +54,21 @@ export class Stage {
 
   startRendering() {
     const ctx = checkNotNull(this.view.getContext("2d"));
-    ctx.scale(1 / window.devicePixelRatio, 1 / window.devicePixelRatio);
+    // ctx.scale(1 / window.devicePixelRatio, 1 / window.devicePixelRatio);
     requestAnimationFrame(() => this.render(ctx));
   }
 
   render(ctx: CanvasRenderingContext2D) {
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+    ctx.save();
+    ctx.translate(WIDTH / 2, HEIGHT / 2);
     for (const sprite of this.sprites.values()) {
+      ctx.save();
+      ctx.translate(-WIDTH / 2, -HEIGHT / 2);
       sprite.render(ctx);
+      ctx.restore();
     }
+    ctx.restore();
     requestAnimationFrame(() => this.render(ctx));
   }
 }
@@ -69,12 +76,15 @@ export class Stage {
 export class Sprite {
   costumes: Costume[] = [];
   scripts = new Set<Script>();
+  x = 0;
+  y = 0;
+  direction = 90;
 
   private nextCostumeId = 1;
   private canvas: HTMLCanvasElement;
   private canvasCtx: CanvasRenderingContext2D;
 
-  constructor() {
+  constructor(public stage: Stage) {
     // An offscreen canvas used when loading sprites, et
     this.canvas = document.createElement("canvas");
     this.canvas.width = WIDTH;
@@ -109,12 +119,16 @@ export class Sprite {
   }
 
   render(ctx: CanvasRenderingContext2D) {
-    ctx.drawImage(this.costumes[0].bitmap, 0, 0);
+    const hw = this.costumes[0].bitmap.width / 2;
+    const hh = this.costumes[0].bitmap.height / 2;
+    ctx.drawImage(this.costumes[0].bitmap, this.x, this.y);
   }
 
   // Movement
 
-  move(steps: number) {}
+  move(steps: number) {
+    this.x += steps;
+  }
 
   turnClockwise(degrees: number) {}
 
@@ -128,7 +142,14 @@ export class Sprite {
 
   // Events
 
-  whenKeyPressed(key: string, callback: (self: Sprite) => void) {}
+  whenKeyPressed(key: string, callback: (sprite: Sprite) => void) {
+    // TODO: Should this actually be throttled by frame?
+    window.addEventListener("keypress", (e: KeyboardEvent) => {
+      if (e.code.toLowerCase() === key) {
+        callback(this);
+      }
+    });
+  }
 }
 
 export const stage = new Stage();
